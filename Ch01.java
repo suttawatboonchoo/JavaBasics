021c00751149dab6b902ef1225a1848c00b97937
User1 <user1@example.com>
2024-09-26 16:49:51 +0700

    Crate Class 01

--- Ch01.java
+++ Ch01.java
@@ -6,6 +6,6 @@ package JavaBasics;
 public class Ch01 {
 
     public static void main(String[] args) {
-        System.out.println("ทดสอบระบบ");
+        System.out.println("ทดสอบระบบ นำข้อความขึ้น git");
     }
 }
