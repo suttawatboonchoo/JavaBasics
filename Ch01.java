c838a76501bdbc059eefd3dd39a46abfa6eaa12d
User1 <user1@example.com>
2024-09-26 17:23:34 +0700

    Class Variables

--- Ch01.java
+++ Ch01.java
@@ -4,8 +4,10 @@ package JavaBasics;
  * Ch01
  */
 public class Ch01 {
-
+ //ตั้งชื่อ Class ต้องตัวแรกตัวใหญ่
+ public static int a = 10;
     public static void main(String[] args) {
-        System.out.println("ทดสอบระบบ นำข้อความขึ้น git");
+
+        System.out.println("Hello Java >> " + a);
     }
 }
