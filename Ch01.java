146064cd9998113a78a77ec24783f37b611b9563
User1 <user1@example.com>
2024-10-03 15:20:07 +0700

    Test Mode

--- Ch01.java
+++ Ch01.java
@@ -6,8 +6,11 @@ package JavaBasics;
 public class Ch01 {
  //ตั้งชื่อ Class ต้องตัวแรกตัวใหญ่
  public static int a = 10;
+ public static char C = 'J';
+
     public static void main(String[] args) {
 
-        System.out.println("Hello Java >> " + a);
+        System.out.println("Java Int >> " + a);
+        System.out.println("Java Char >> " + C);
     }
 }
